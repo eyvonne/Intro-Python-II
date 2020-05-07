d22fd4947d97ec101df610f106abe0524d4a2896
eyvonne <eyvonne@users.noreply.github.com>
2020-05-07 12:40:17 -0700

    added items, populating rooms not working

--- src/adv.py
+++ src/adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
-
+from item import Item
+from random import choice
 # Declare all the rooms
 
 room = {
@@ -37,7 +38,26 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
+# make items that are going to be distributed into the rooms
+stuff = [
+    Item('stick', 'a simple wooden stick, could be used to hit something'),
+    Item('rock', 'a rock, what more do you need to know?'),
+    Item('boulder', "a large rock, you really shouldn't be able to carry this around"),
+    Item('treasure chest', "A chest full of treatures beyond your wildest imagination"),
+    Item('sword', 'a long metal object that looks like it could be used for slicing')
+]
 
+# randomly place the items in the rooms
+for i in stuff:
+    print(i)
+    r = choice(list(room))
+    print(r)
+    print(room[r].name)
+    room[r].add_item(i)
+    print(room[r].treasure)
+
+for _, r in room.items():
+    print(_, r.treasure)
 # Make a new player object that is currently in the 'outside' room.
 play = Player('Adventurer', room['outside'])
 
@@ -57,7 +77,10 @@ print(f'hello {play.name}')
 while playon != 'q':
     print(f"You're in the room: {play.current_room.name}")
     print(play.current_room.desc)
-    playon = input('Which Way Would You Like To Go?')
+    if len(play.current_room.treasure):
+        print('In the room you see: ', end='')
+        print(*play.current_room.treasure, sep=', ')
+    playon = input('Which Way Would You Like To Go? ')
 
     if playon.lower() == 'n' or playon.lower() == 'north':
         play.current_room = play.current_room.n_to
--- src/player.py
+++ src/player.py
@@ -1,8 +1,23 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from typing import List
+from item import Item
 
 
 class Player():
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, inventory: List[Item] = []):
         self.name = name
         self.current_room = current_room
+        self.inventory = inventory
+
+    def pick_up(self, item: str):
+        for i in self.current_room.items:
+            if i.name == item.lower():
+                self.current_room.items.remove(i)
+                self.inventory.append(i)
+
+    def drop(self, item: str):
+        for i in self.inventory:
+            if i.name == item.lower():
+                self.inventory.remove(i)
+                self.current_room.items.append(i)
